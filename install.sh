7b9e9d729bd24806346c7705ea4a54e74ce6bcd5
TermuxDev <termuxdev.00@gmail.com>
2024-11-08 07:34:11 -0500

    TypeScript Added

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ yes|pkg install mongodb
 
 # INSTALANDO MÓDULOS DE NPM
 
-npm install -g live-server localtunnel prettier vercel
+npm install -g typescript live-server localtunnel prettier vercel
 
 # SOLUCIONANDO ERROR DE (lt) OPENURL.JS
 
